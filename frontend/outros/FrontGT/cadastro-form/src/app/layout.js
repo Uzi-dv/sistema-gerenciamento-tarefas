0b10afa48e5fff18c7ba3412081f5c77d1704005
Pedro Henrique <hlima8703@gmail.com>
2025-03-23 15:43:59 -0300

    .

--- frontend/outros/FrontGT/cadastro-form/src/app/layout.js
+++
@@ -1,8 +0,0 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-export default function RootLayout({ children }) {
-  return (
-    <html lang="pt-BR">
-      <body suppressHydrationWarning={true}>{children}</body>
-    </html>
-  );
-}
